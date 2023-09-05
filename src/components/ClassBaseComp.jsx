8ee81ad356a61a3c0c44572f04a77eb78e2e9e68
gaurav bisht <gauravengg.bisht@gmail.com>
2023-09-05 15:51:13 +0530

    classbase component updated

--- src/components/ClassBaseComp.jsx
+++ src/components/ClassBaseComp.jsx
@@ -10,20 +10,11 @@ class Example extends React.Component{
     }
 
     render(){
-        const {count1}= this.state;
-        const {count2}=this.state;
+        
         return(
-            <div className="container">
-                    <h1>count1:{count1}</h1>
-                    <h1>count2:{count2}</h1>
-                    
-                <button onClick={()=>{
-                    this.setState({
-                        count1:1,
-                        count2:2
-                    })
-                }}>button</button>
-            </div>
+           <div>
+            <h1>Nothing</h1>
+           </div>
         )
     }
 }
